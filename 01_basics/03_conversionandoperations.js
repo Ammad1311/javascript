a3ae0f0da1c8eff5c07b1eeeac800d238e903d3e
Ammad1311 <161191042+Ammad1311@users.noreply.github.com>
2024-06-14 13:36:08 +0000

    03_conversionandoperations.js

---
+++ 01_basics/03_conversionandoperations.js
@@ -0,0 +1,9 @@
+let score = "1311abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
