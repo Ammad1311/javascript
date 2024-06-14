d43786311bfcbcbe04ce8f2bdda4c086d8900f2d
Ammad1311 <161191042+Ammad1311@users.noreply.github.com>
2024-06-14 12:38:39 +0000

    01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail ="akhdhdj1267@gmail.com"
+var accountPassword = "gcdhc6562"
+
+accountEmail = "cdjcdh@gmail.com"
+accountPassword = "jdej2324"
+accountCity ="Islamabad"
+
+
+console.log("accounId, accountEmail, accountPassword")
+console.table([accountEmail, accountPassword, accountCity])
